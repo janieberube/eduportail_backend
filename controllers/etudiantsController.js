2ad2506818e8a3cbf81d2da625d94404e3db3aea
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-04 21:26:30 -0300

    Update etudiantsController.js
    Modified indentation

--- controllers/etudiantsController.js
+++ controllers/etudiantsController.js
@@ -5,33 +5,31 @@ const pool = require('./config/database');
 
 // Controller method to retrieve all etudiants
 exports.getTousLesEtudiants = (req, res) => {
-
-  pool.query('SELECT * FROM etudiants', (error, results) => {
-    if (error) {
-      console.error('Error fetching etudiants:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-    res.status(200).json(results);
-  });
+    pool.query('SELECT * FROM etudiants', (error, results) => {
+        if (error) {
+            console.error('Error fetching etudiants:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+        res.status(200).json(results);
+    });
 };
 
 // Controller method to retrieve etudiant by matricule
 exports.getEtudiantParMatricule = (req, res) => {
-  const matricule = req.params.matricule;
-
-  pool.query('SELECT * FROM etudiants WHERE id = ?', [matricule], (error, results) => {
-    if (error) {
-      console.error('Error fetching etudiant:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-    if (results.length === 0) {
-      res.status(404).json({ error: 'Étudiant introuvable' });
-      return;
-    }
-    res.status(200).json(results[0]);
-  });
+    const matricule = req.params.matricule;
+    pool.query('SELECT * FROM etudiants WHERE id = ?', [matricule], (error, results) => {
+        if (error) {
+            console.error('Error fetching etudiant:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+        if (results.length === 0) {
+            res.status(404).json({ error: 'Étudiant introuvable' });
+            return;
+        }
+        res.status(200).json(results[0]);
+    });
 };
 
 /*
