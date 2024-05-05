f962cf2aa4f69743be5b5be47cd84214a3fa46f1
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-04 22:44:46 -0300

    Corrected etudiantsRoute.js spelling mistake

--- routes/etudiantsRoutes.js
+++ routes/etudiantsRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const etudiantsController = require('../controllers/etudiantsController');
 
 // Define routes
-router.get('/etudiants', etudiantsController.getTousLesEtudiant);
+router.get('/etudiants', etudiantsController.getTousLesEtudiants);
 router.get('/etudiants/:matricule', etudiantsController.getEtudiantParMatricule);
 
 module.exports = router;
