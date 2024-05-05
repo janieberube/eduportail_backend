f56f061d1878d93ce324e980f35eda99ebac616a
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-04 22:52:43 -0300

    Update server.js
    Added API namespace

--- server.js
+++ server.js
@@ -2,44 +2,23 @@
 // set up a HTTP server using the Express.js framework
 
 const express = require('express');
+const etudiantsRoutes = require('./routes/etudiantsRoutes');
 
 // Create Express application
 const app = express();
 
 // Define a route 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.send('Successfully running server.js file, yihoo!');
   });
 
+// Define routes with the /api namespace
+app.use('/api', etudiantsRoutes);
 
-/*
-//COURS
-// Define a route to fetch courses for a student from the database
-app.get('/api/courses/:matricule', (req, res) => {
-    const { matricule } = req.params;
-    
-    if (!matricule) {
-      res.status(400).json({ error: 'Matricule is required' });
-      return;
-    }
-  
-    const sql = 'SELECT * FROM inscriptions WHERE Etudiant_matricule = ?';
-    connection.query(sql, [matricule], (error, results) => {
-      if (error) {
-        console.error('Error fetching courses:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-      }
-      res.json(results);
-    });
-  });
-  */
+// Start the server 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-  // Start the server 
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-
-  // This file enables us to run the Node.js server (using the command 'node server.js in command prompt')
-  
+// This file enables us to run the Node.js server (using the command 'node server.js in command prompt')
