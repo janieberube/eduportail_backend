b2cd846c43e0e9b6c5e34ef18b19230404eb43c2
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-04 21:35:10 -0300

    Update coursRoutes.js
    Modification du nom du fichier controller dans le code

--- routes/coursRoutes.js
+++ routes/coursRoutes.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const router = express.Router();
-const courseController = require('../controllers/courseController');
+const courseController = require('../controllers/coursController');
 
 // Define routes
 router.get('/courses', coursController.getTousLesCours);
