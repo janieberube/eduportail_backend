72782fe8fdf243c00baa4fa35d5cdc5c022217c4
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-07 11:51:14 -0300

    Update server.js
    Modification des commentaires, ajout de la route pour coursRoutes

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
-// server.js
-// set up a HTTP server using the Express.js framework
-// MVC (Model-View-Controller) architecture pattern
+// Fichier:             server.js
+// Programmeurs:        Janie Bérubé, Clément Sonier, André Doucet
+// Description:         Configuration d'un serveur HTTP avec le Express.js framework
+//                      MVC (Model-View-Controller) architecture pattern
+//                      Permet d'exécuter le serveur Node.js (commande au terminal: node server.js)
+
 
 const express = require('express');
 const etudiantsRoutes = require('./routes/etudiantsRoutes');
+const coursRoutes = require('./routes/coursRoutes');
 
-// Create Express application
+
+// Créer une application Express
 const app = express();
 
-// Define a route 
+
+// Définir une route 
 app.get('/', (req, res) => {
-    res.send('Successfully running server.js file, yihoo!');
+    res.send('Exécution réussie du fichier server.js');
   });
 
-// Define routes with the /api namespace
-app.use('/api', etudiantsRoutes);
 
-// Start the server 
+// Définir les routes avec le namspace /api
+app.use('/api', etudiantsRoutes);
+app.use('/api', coursRoutes);
+
+
+// Commencer le serveur
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Le serveur fonctionne sur le port ${port}`);
 });
 
-// This file enables us to run the Node.js server (using the command 'node server.js in command prompt')
