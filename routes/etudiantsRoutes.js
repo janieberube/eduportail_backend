5a13c39b684c6a3ec10c85865eef2972454984cf
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-07 10:29:19 -0300

    Update etudiantsRoutes.js
    Modification des commentaires

--- routes/etudiantsRoutes.js
+++ routes/etudiantsRoutes.js
@@ -1,12 +1,18 @@
-// etudiantsRoutes.js
-// API route
+// Fichier:             etudiantsRoutes.js
+// Programmeurs:        Janie Bérubé, Clément Sonier, André Doucet
+// Description:         Module de route pour une API RESTful dans l'application Node.js avec le framework Express
+// Bût:                 Définir les routes disponibles pour accéder aux fonctionnalités liées aux étudiants   
 
+
+// Importation des modules
 const express = require('express');
 const router = express.Router();
 const etudiantsController = require('../controllers/etudiantsController');
 
-// Define routes
+// Définition des routes, association aux méthodes correspondantes des controlleurs
 router.get('/etudiants', etudiantsController.getTousLesEtudiants);
 router.get('/etudiants/:matricule', etudiantsController.getEtudiantParMatricule);
 
+
+// Exportation du router pour pouvoir être utilisé ailleurs dans l'application
 module.exports = router;
