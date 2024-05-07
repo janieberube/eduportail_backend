35dd0209bfcd0af5032e37cd73bb4e9d06016886
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-07 20:45:53 -0300

    Create evaluationsModel.js

---
+++ models/evaluationsModel.js
@@ -0,0 +1,44 @@
+// Fichier:             evaluationsModel.js
+// Programmeurs:        Janie Bérubé, Clément Sonier, André Doucet
+// Bût:                 Défini le schéma (définir la structure des données) et l'interaction avec les tables pours les cours
+
+/*
+Modèle :                Gère les données et la logique métier de l'application. 
+
+                        Il est responsable de définir comment les données sont structurées et manipulées
+                        et inclut des fonctions pour interagir avec la bd pour effectuer 
+                        des opérations CRUD (Create, Read, Update, Delete). 
+*/
+
+// Importation du module de configuration de la base de données distante
+const pool = require('../config/database'); 
+
+
+// Fonction pour récupérer les évaluations par le matricule étudiant
+module.exports.getEvaluationsParMatricule = (matricule) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT evaluations_etudiants.datePublication, evaluations_etudiants.notePointage, evaluations_etudiants.notePourcentage,
+                    evaluations.nomEvaluation, evaluations.ponderation, evaluations.pointage
+                    FROM evaluations_etudiants 
+                    INNER JOIN evaluations
+                    ON evaluations_etudiants.Evaluations_idEvaluation = evaluations.idEvaluation
+                    INNER JOIN inscriptions
+                    ON evaluations_etudiants.Inscriptions_idInscription = inscriptions.idInscription
+                    WHERE inscriptions.Etudiant_matricule = ?
+                    AND evaluations_etudiants.publier = '1'
+                    ORDER BY evaluations_etudiants.datePublication DESC`, [matricule], (error, results) => {
+            if (error) {
+                console.error('Erreur lors de la récupération des évaluations pour l\'étudiant:', error);
+                reject(error);
+                return;
+            }
+            resolve(results);
+        });
+    });
+};
+
+
+
+
+
+
