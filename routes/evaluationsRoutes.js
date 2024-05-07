7447cbb1d2c4b8042a7e8b83bdddceef3126c65f
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-07 20:59:53 -0300

    Create evaluationsRoutes.js

---
+++ routes/evaluationsRoutes.js
@@ -0,0 +1,18 @@
+// Fichier:             evaluationsRoutes.js
+// Programmeurs:        Janie Bérubé, Clément Sonier, André Doucet
+// Description:         Module de route pour une API RESTful dans l'application Node.js avec le framework Express
+// Bût:                 Définir les routes disponibles pour accéder aux fonctionnalités liées aux évaluations     
+
+
+// Importation des modules
+const express = require('express'); 
+const router = express.Router();
+const evaluationsController = require('../controllers/evaluationsController');
+
+
+// Définition des routes, association aux méthodes correspondantes des controlleurs
+router.get('/evaluations/:matricule', evaluationsController.getEvaluationsParMatricule);
+
+
+// Exportation du router pour pouvoir être utilisé ailleurs dans l'application
+module.exports = router;
