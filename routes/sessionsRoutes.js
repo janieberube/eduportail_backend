bef8e4e50287444945e4bc95dab35450efa82cfe
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-07 21:42:35 -0300

    Create sessionsRoutes.js

---
+++ routes/sessionsRoutes.js
@@ -0,0 +1,17 @@
+// Fichier:             sessionsRoutes.js
+// Programmeurs:        Janie Bérubé, Clément Sonier, André Doucet
+// Description:         Module de route pour une API RESTful dans l'application Node.js avec le framework Express
+// Bût:                 Définir les routes disponibles pour accéder aux fonctionnalités liées aux sessions 
+
+
+// Importation des modules
+const express = require('express');
+const router = express.Router();
+const sessionsController = require('../controllers/sessionsController');
+
+// Définition des routes, association aux méthodes correspondantes des controlleurs
+router.get('/sessions/:matricule', sessionsController.getSessionsParMatricule);
+
+
+// Exportation du router pour pouvoir être utilisé ailleurs dans l'application
+module.exports = router;
