9a6b978ef1f7c8f8ca0137e6a54a60403ed64b2d
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-08 10:29:27 -0300

    Create enseignantsModel.js

---
+++ models/enseignantsModel.js
@@ -0,0 +1,35 @@
+// Fichier:             enseignantsModel.js
+// Programmeurs:        Janie Bérubé, Clément Sonier, André Doucet
+// Bût:                 Défini le schéma (définir la structure des données) et l'interaction avec les tables pours les enseignants
+
+
+// Importation du module de configuration de la base de données
+const pool = require('../config/database'); 
+
+
+// Fonction pour récupérer un enseignant par le id de l'évaluation pour un étudiant
+exports.getEnseignantParIdEvalEtudiant = (matricule) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT enseignants.prenom, enseignants.nom
+                    FROM enseignants
+                    INNER JOIN cours_livres ON cours_livres.Enseignants_idEnseignant = enseignants.idEnseignant
+                    INNER JOIN inscriptions ON inscriptions.Cours_Livres_idCours_Livre = cours_livres.idCours_Livre
+                    INNER JOIN evaluations_etudiants ON evaluations_etudiants.Inscriptions_idInscription = inscriptions.idInscription
+                    INNER JOIN evaluations ON evaluations.idEvaluation = evaluations_etudiants.Evaluations_idEvaluation
+                    WHERE evaluations_etudiants.idEvaluation_Etudiant = ?`, [matricule], (error, results) => {
+            if (error) {
+                console.error('Erreur lors de la récupération de l\'enseignant:', error);
+                reject(error);
+                return;
+            }
+            if (results.length === 0) {
+                resolve(null);          // Enseignant introuvable
+            }
+            else {
+                resolve(results[0]);    // Retourne le premier enseignant trouvé
+            }
+        });
+    });
+};
+
+
