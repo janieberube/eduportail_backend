59d729c62d830b3f165952f076c15d24000dad61
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-09 16:58:41 -0300

    Create moyenneSouhaiteeRoutes.js

---
+++ routes/moyenneSouhaiteeRoutes.js
@@ -0,0 +1,17 @@
+// Fichier:             moyenneSouhaiteeRoutes.js
+// Programmeurs:        Janie Bérubé, Clément Sonier, André Doucet
+// Description:         Module de route pour une API RESTful dans l'application Node.js avec le framework Express
+// Bût:                 Définir les routes disponibles pour accéder à la moyenne souhaitée  
+
+
+// Importation des modules
+const express = require('express');
+const router = express.Router();
+const moyenneSouhaiteeController = require('../controllers/moyenneSouhaiteeController');
+
+// Définition des routes, association aux méthodes correspondantes des controlleurs
+router.get('/moyenneSouhaitee/:matricule', moyenneSouhaiteeController.getMoyenneSouhaiteeParMatricule);
+
+
+// Exportation du router pour pouvoir être utilisé ailleurs dans l'application
+module.exports = router;
