f10b6006136bc3a9bd7f410e8c2920c7d9b8e7f0
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-09 16:53:32 -0300

    Create moyenneSouhaiteeController.js

---
+++ controllers/moyenneSouhaiteeController.js
@@ -0,0 +1,32 @@
+// Fichier:             etudiantsController.js
+// Programmeurs:        Janie Bérubé, Clément Sonier, André Doucet
+// Bût:                 Gérer la logique liée à la moyenne souhaitee
+
+
+// Importation du module de configuration de la base de données 
+const pool = require('../config/database'); 
+
+
+// Méthode du contrôleur pour récupérer une moyenne souhaitée par le matricule
+exports.getMoyenneSouhaiteeParMatricule = (req, res) => {
+    const matricule = req.params.matricule;
+    pool.query(`SELECT moyenneSouhaitee
+                FROM inscriptions_sessions
+                INNER JOIN sessions ON sessions.sessionID = inscriptions_sessions.Sessions_sessionID
+                WHERE CURRENT_DATE() >= sessions.dateDebutSession 
+                AND CURRENT_DATE() <= sessions.dateFinSession
+                AND inscriptions_sessions.Etudiants_matricule = ?`, [matricule], (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la récupération de la moyenne souhaitée:', error);
+            res.status(500).json({ error: 'Erreur lors de la récupération de la moyenne souhaitée: ' + error.message });
+            return;
+        }
+        if (results.length === 0) {
+            res.status(404).json({ error: 'Moyenne souhaitée introuvable' });
+            return;
+        }
+        res.status(200).json(results[0]);
+    });
+};
+
+
