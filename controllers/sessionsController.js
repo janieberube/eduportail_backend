cf9e2900b0c1b66a3e77dff2f46571e4be514c62
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-10 16:08:40 -0300

    Update sessionsController.js
    Modification sessionActuelle pour prendre le matricule en parametre correctement

--- controllers/sessionsController.js
+++ controllers/sessionsController.js
@@ -25,26 +25,24 @@ exports.getSessionsParMatricule = (req, res) => {
 };
 
 // Méthode du contrôleur pour récupérer la session actuelle par le matricule
-exports.getSessionActuelleParMatricule = (matricule) => {
-    return new Promise((resolve, reject) => {
-        pool.query(`SELECT sessions.nomSession
-                    FROM sessions
-                    INNER JOIN inscriptions_sessions
-                    ON sessions.sessionID = inscriptions_sessions.Sessions_sessionID
-                    WHERE inscriptions_sessions.Etudiants_matricule = ?
-                    AND CURRENT_DATE() >= sessions.dateDebutSession 
-                    AND CURRENT_DATE() <= sessions.dateFinSession`, [matricule], (error, results) => {
-            if (error) {
-                console.error('Erreur lors de la session:', error);
-                reject(error);
-                return;
-            }
-            if (results.length === 0) {
-                resolve(null);          // Session introuvable
-            }
-            else {
-                resolve(results[0]);    // Retourne la session actuelle
-            }
-        });
+exports.getSessionActuelleParMatricule = (req, res) => {
+    const matricule = req.params.matricule; 
+    pool.query(`SELECT sessions.nomSession
+                FROM sessions
+                INNER JOIN inscriptions_sessions
+                ON sessions.sessionID = inscriptions_sessions.Sessions_sessionID
+                WHERE inscriptions_sessions.Etudiants_matricule = ?
+                AND CURRENT_DATE() >= sessions.dateDebutSession 
+                AND CURRENT_DATE() <= sessions.dateFinSession`, [matricule], (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la récupération de la session actuelle:', error);
+            res.status(500).json({ error: 'Erreur lors de la récupération de la session actuelle: ' + error.message });
+            return;
+        }
+        if (results.length === 0) {
+            res.status(404).json({ message: 'Aucune session actuelle trouvée pour ce matricule' });
+        } else {
+            res.status(200).json(results[0]); // Retourne la session actuelle
+        }
     });
 };
