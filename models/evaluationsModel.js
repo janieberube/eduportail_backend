bad31f0633d4f390a443687eeec95e34f2af9263
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-14 20:19:26 -0300

    Ajout fonction pour voir tous les evals (incluant ceux non publiées)
    evaluationsController, evaluationsModel, evaluationsRoutes

--- models/evaluationsModel.js
+++ models/evaluationsModel.js
@@ -168,4 +168,32 @@ module.exports.getCoursFaiblesse = () => {
     });
 };
 
+module.exports.getTousLesEvaluationsParIdInscription = (idInscription) => {
+    const idInscription = req.params.idInscription;
+    pool.query(`SELECT evaluations_etudiants.datePublication, evaluations_etudiants.notePointage, evaluations_etudiants.notePourcentage, evaluations_etudiants.retroaction, types_evaluations.idType_Evaluation, evaluations.nomEvaluation, evaluations.ponderation, evaluations.pointage, cours.sigle
+                FROM evaluations_etudiants 
+                INNER JOIN evaluations
+                ON evaluations_etudiants.Evaluations_idEvaluation = evaluations.idEvaluation
+                INNER JOIN types_evaluations
+                ON types_evaluations.idType_Evaluation = evaluations.Types_Evaluations_idType_Evaluation
+                INNER JOIN inscriptions
+                ON evaluations_etudiants.Inscriptions_idInscription = inscriptions.idInscription
+                INNER JOIN cours_livres
+                ON inscriptions.Cours_Livres_idCours_Livre = cours_livres.idCours_Livre
+                INNER JOIN cours
+                ON cours.sigle = cours_livres.Cours_sigle
+                INNER JOIN sessions
+                ON cours_livres.Sessions_sessionID = sessions.sessionID
+                WHERE inscriptions.idInscription = ?
+                AND CURRENT_DATE() >= sessions.dateDebutSession 
+                AND CURRENT_DATE() <= sessions.dateFinSession
+                ORDER BY evaluations_etudiants.datePublication DESC`, [idInscription], (error, results) => {
+        if (error) {
+            console.error('Erreur lors de la récupération des évaluations:', error);
+            reject(error);
+            return;
+        }
+        resolve(results);
+    });
+};
 
--- routes/evaluationsRoutes.js
+++ routes/evaluationsRoutes.js
@@ -16,6 +16,7 @@ router.get('/evaluations/matricule/:matricule', evaluationsController.getTousLes
 router.get('/evaluations/retroaction/:matricule', evaluationsController.getTousLesEvaluationsAvecRetro);
 router.get('/evaluations/force', evaluationsController.getCoursForce);
 router.get('/evaluations/faiblesse', evaluationsController.getCoursFaiblesse);
+router.get('/evaluations/inscription/tous/:idInscription', evaluationsController.getTousLesEvaluationsParIdInscription);
 
 // Exportation du router pour pouvoir être utilisé ailleurs dans l'application
 module.exports = router;
