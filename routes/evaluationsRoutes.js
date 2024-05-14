e0143138393d1cf8676f3af0b28bd1327fce602e
Janie Bérubé <133725989+janieberube@users.noreply.github.com>
2024-05-14 17:33:11 -0300

    Correction evaluationsRoutes.js (nom des fonctions)

--- routes/evaluationsRoutes.js
+++ routes/evaluationsRoutes.js
@@ -14,8 +14,8 @@ const evaluationsController = require('../controllers/evaluationsController');
 router.get('/evaluations/inscription/:idInscription', evaluationsController.getEvaluationsParIdInscription);
 router.get('/evaluations/matricule/:matricule', evaluationsController.getTousLesEvaluationsPourEtudiant);
 router.get('/evaluations/retroaction/:matricule', evaluationsController.getTousLesEvaluationsAvecRetro);
-router.get('/evaluations/force', evaluationsController.getTousCoursForce);
-router.get('/evaluations/faiblesse', evaluationsController.getTousCoursFaiblesse);
+router.get('/evaluations/force', evaluationsController.getCoursForce);
+router.get('/evaluations/faiblesse', evaluationsController.getCoursFaiblesse);
 
 // Exportation du router pour pouvoir être utilisé ailleurs dans l'application
 module.exports = router;
